f263ef7bb5ec4526fe9e97daac9208608f91864c
Christian Roier <christianroier@hotmail.com>
2023-09-02 16:03:45 +0100

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,6 +46,21 @@ puts "Adding jobs and favourites to database"
     video_url: "kc2acmjfrjpy52py5izf"
   )
 
+puts "Creating tags"
+
+Tag.create!(name: "Information Technology (IT)")
+Tag.create!(name: "Healthcare")
+Tag.create!(name: "Education")
+Tag.create!(name: "Business and Finance")
+Tag.create!(name: "Creative and Media")
+Tag.create!(name: "Engineering and Manufacturing")
+Tag.create!(name: "Customer Service and Sales")
+Tag.create!(name: "Science and Research")
+Tag.create!(name: "Transportation and Logistics")
+Tag.create!(name: "Construction and Trades")
+Tag.create!(name: "Hospitality and Tourism")
+Tag.create!(name: "Legal and Law Enforcement")
+Tag.create!(name: "Social Services and Nonprofits")
 
 puts "Finished!"
 
