ce328a69c8a5f83c897ff2bb84ac30641841156d
Jake Wright <jaketwright0@gmail.com>
2023-09-02 14:30:21 +0100

    Added migration to add user reference to jobs table

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,13 +18,14 @@ class JobsController < ApplicationController
       # redirect_to profile or index page
     else
       render :new, status: :unprocessable_entity
+    end
   end
 
   private
     def job_params
       params.require(:job).permit(:title, :average_salary, :duties, :best_bits, :worst_bits, :qualifications, :video_url, :description_tags)
     end
-    
+
     def set_job
       @job = Job.find(params[:id])
     end
