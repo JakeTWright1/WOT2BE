44dd883c904a217e77daff8405a1a5b106a2a3c9
Jake Wright <jaketwright0@gmail.com>
2023-09-02 12:53:51 +0100

    added show method to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,16 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: :show
 
   def index
     @jobs = Job.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
   end
 end
