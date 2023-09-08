38b7c3dc11eecae2e7371b3fd97a3024b38c9be9
Jake Wright <jaketwright0@gmail.com>
2023-09-08 10:23:18 +0100

    username added to users column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_132514) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_091513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_132514) do
   create_table "favourites", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "job_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["job_id"], name: "index_favourites_on_job_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "jobs", force: :cascade do |t|
@@ -82,11 +86,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_132514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "worker"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favourites", "jobs"
+  add_foreign_key "favourites", "users"
   add_foreign_key "jobs", "users"
 end
