7a76968fc734283fa9d90fbb3f6af014311f863c
Jake Wright <jaketwright0@gmail.com>
2023-09-09 17:50:45 +0100

    video player

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_091513) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_09_09_101829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_091513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "video_public_id"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
